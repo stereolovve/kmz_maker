001936a58b92088c9c77a064aca7ed350f32a900
root <root@ip-172-31-1-143.sa-east-1.compute.internal>
2025-11-24 21:40:28 +0000

    allow host

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY', default="django-insecure-d%0xp2!u5w)8*j&zcodho
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = ["*"]
 
 # Google Maps API Key (para processamento com rotas)
 GOOGLE_MAPS_API_KEY = config('GOOGLE_MAPS_API_KEY', default='')
